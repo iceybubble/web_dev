7fbbbced74e5c4d562a2df319d9f078dd930e980
Pallavi Kathait <kathaitpallavi164@gmal.com>
2024-10-20 21:13:32 +0530

    added js Files

---
+++ first.js
@@ -0,0 +1,40 @@
+/*console.log("Pallavi Kathait");
+
+const age = 24;
+let FullName = "Tony Stark";
+let price = 245.54
+console.log(FullName,price);
+x = null;
+y = undefined;
+console.log(x,y);
+
+
+let isFollow = false;
+console.log(isFollow);
+
+const PI=3.14
+console.log(PI)
+ 
+{
+    let a=5;
+    console.log(a);
+}
+{
+    let a = 23;
+    console.log(a);
+}
+
+let x = BigInt(123);
+let y= Symbol(hello!)*/
+
+const student= {
+    name:"Priyanshi Kathait",
+    age: 20,
+    cgpa: 8,
+    isPass: true
+};
+
+student ["age"] = ["22"];
+console.log(student);
+console.log(student. age);
+
---
+++ practice.js
@@ -0,0 +1,8 @@
+const Product = {
+    name: "Pen",
+    price : 270,
+    Model : "Parker Jotter",
+    Ink : "blue",
+    Rating : 4
+};
+console.log(Product);
